22c174e5a125cd625c4d7488a6302fb58607daac
Leon <lionhive@gmail.com>
2017-08-04 05:55:00 +0000

    migrate & update simons story

---
+++ src/application/views/general-pages/simons-story.php
@@ -0,0 +1,55 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div class="container">
+<div class="row">
+<div class="funding">
+<h1>Simon&rsquo;s story</h1>
+
+<div class="row">
+<div class="col-lg-3 col-md-4 col-sm-5 col-xs-12 simonleft"><img alt="" src="/images/img16.jpg" /></div>
+
+<div class="col-lg-9 col-md-8 col-sm-7 col-xs-12 simonright">
+<p><strong>In 1993,</strong> I fell&nbsp;behind in my rent and was evicted. I had a job, but for the next 4 months I&nbsp;lived in my car while I saved up enough money&nbsp;for a month's bond and a month's&nbsp;rent on another place. I would park in a car park near my old place for the&nbsp;night, and in the mornings,&nbsp;drive to a caravan park near by, sneak in, have a&nbsp;shower and go to work.</p>
+
+<p>Since then, I have&nbsp;made a good living for years as a Chef, and Entrepreneur, for the most part&nbsp;living selfishly and not giving a second&nbsp;thought to homelessness.</p>
+
+<p>In May 2015, I was&nbsp;walking along Carlisle Street in St. Kilda East, Melbourne, to my local Coles&nbsp;supermarket. As I approached the brand&nbsp;new Bank of Melbourne I see a bright,&nbsp;white doona crumpled up in the tiny alcove of an unused doorway. As I got closer&nbsp;I noticed there&nbsp;was a man curled up in the doona, on the hard concrete floor, trying&nbsp;to get some sleep&hellip; at lunch time.</p>
+
+<p>So many people were&nbsp;walking past, looking, but moving on with their day, as I have probably done&nbsp;since 1993.</p>
+
+<h2>This time I couldn&rsquo;t walk&nbsp;past, I stopped and asked&nbsp;him if he was ok&hellip;</h2>
+
+<p>he said &ldquo;yeah mate, thanks,&nbsp;just trying to get some sleep&rdquo;. He looked so tired. I said, &ldquo;here mate&rdquo; and gave&nbsp;him the $20 I had in my pocket.</p>
+
+<h2>His eyes&nbsp;lit up, he smiled and was so grateful.&nbsp;</h2>
+
+<p>He shook my hand, thanked me again with a smile and curled back up under the&nbsp;doona. When I got home, I told my family what&nbsp;had happened and tears rolled&nbsp;down my face.</p>
+
+<p>That man, trying to&nbsp;sleep on a concrete floor, in the middle of the day, on a busy city street&nbsp;affected me in a profound way. And that&rsquo;s a&nbsp;mild story, for many sleeping on&nbsp;the streets are being subjected to terrible weather, harassment, bullying,&nbsp;being robbed and worse. No&nbsp;one should have to live like that.</p>
+
+<h3>What is charity?</h3>
+
+<p>For me, charity is&nbsp;practical. It&#39;s the ability to use one&#39;s position of influence, relative wealth&nbsp;and power to affect lives for the better.</p>
+
+<p>I&rsquo;m not a religious&nbsp;person, but there&#39;s a story in the bible about a man beaten near death by&nbsp;robbers. He&#39;s stripped naked and lying on&nbsp;the roadside. Most people pass him&nbsp;by, but one man stops. He picks him up and bandages his wounds. He puts him on&nbsp;his horse and&nbsp;walks alongside until they reach an inn. He checks him in and&nbsp;throws down his Amex. &quot;Whatever he needs until he gets better.&rdquo;</p>
+
+<p>Because he could.</p>
+
+<p>The dictionary&nbsp;defines charity as simply:</p>
+
+<h2>The act of giving voluntarily to those in need.</h2>
+
+<p>Using my 20+ years of&nbsp;business experience, I set about developing a sustainable business model with a&nbsp;simple mission; to provide those&nbsp;sleeping &ldquo;rough&rdquo;, a safe overnight place to&nbsp;sleep. The more I developed and researched a solution, the more I discovered&nbsp;what a good&nbsp;nights sleep can do for a persons physical and mental health.&nbsp;Just being able to sleep through the night,&nbsp;warm and safe can give a person a whole new outlook on life.</p>
+
+<p>The least we can do&nbsp;is&nbsp;bring safe overnight accommodation&nbsp;to people sleeping rough in Australia&nbsp;until they get back on their feet.</p>
+
+<p><b>Simon Rowe</b><br />
+<bdo dir="ltr">Winter&nbsp;2015</bdo></p>
+
+<p><a href="/meet-the-board">&lt; Back to Meet the board</a></p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
