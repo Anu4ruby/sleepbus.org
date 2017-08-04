9b25632c25aa8821a0a83989c226ef627b8adb26
Leon <lionhive@gmail.com>
2017-08-04 08:27:29 +0000

    fix btn click on #54

--- src/application/views/general-pages/about-us.php
+++ src/application/views/general-pages/about-us.php
@@ -92,7 +92,10 @@
 <a class="btn btn-info" href="/fundraise">Fundraise</a></div>
 </div>
 </div>
+<br />
+<br />
 </div>
+
 
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 <div class="container">
