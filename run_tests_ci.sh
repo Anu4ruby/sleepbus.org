d4ed4634f2fecf38ed8115472f9275c5dc79d2c7
Leon <lionhive@gmail.com>
2017-08-16 02:23:28 +0000

    quieten docker build

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -3,8 +3,8 @@
 # source env vars to use in Docker run commands
 . ./env-vars
 
-sudo docker build -t sleepbus/web:latest . 
-sudo docker build -t sleepbus/test:latest ./test
+sudo docker build -q -t sleepbus/web:latest . 
+sudo docker build -q -t sleepbus/test:latest ./test
 
 sudo docker run --name sleepbussql -e MYSQL_ROOT_PASSWORD=$SLEEPBUS_DB_PASSWORD -d mariadb
 sudo docker run --name sleepbusweb --link sleepbussql:mysql --env-file ./env-vars -p 8080:80 -d -v $(pwd)/src/:/app sleepbus/web
