a82c0d1056075a9c73f33d59e08b7954cf5e2314
Leon <lionhive@gmail.com>
2017-09-05 14:41:45 +0800

    add refund policy, linked from faq

---
+++ src/application/views/general-pages/refund-policy.php
@@ -0,0 +1,80 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+  <div class="container">
+    <div class="row">
+	<div class="funding">
+
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+<div class="privacy">
+<h1>Refund Policy for sleepbus</h1>
+
+<h2 style="text-align: left;padding: 0px;">Purpose</h2>
+
+
+<p>This policy is intended to inform people who have made a donation towards sleepbus on what to do if they have made a donation in error or a mistake in the amount of the donation.</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Applies to</h2>
+
+<p>
+This policy applies to all donations made to sleepbus Ltd.
+</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Online Donations</h2>
+
+<p>
+Thank you for choosing to donate to sleepbus.  Please make sure you complete all details carefully, including the correct amount you are donating to us.
+</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Errors in Amount Pledged / Duplicate Donation</h2>
+
+<p>
+If you have made an error in the amount of your donation or made a duplicate donation, please contact us as soon as possible. All requests for refunds must be made in writing via email and forwarded to <a href="mailto:info@sleepbus.org">info@sleepbus.org</a>. You have 60 days in which to notify sleepbus that an error has been made.
+</p>
+
+<p>
+Should the refund be approved, any original receipt issued for the incorrect amount immediately becomes void and invalid. sleepbus will issue a new receipt where applicable.
+</p>
+
+<p> 
+sleepbus is a non-profit charity organisation and we reserve the right to deduct any bank or transaction charges for any refund processed onto the donor. Should an error be detected and a request for refund made after 90 days or after the end of the prior financial year, sleepbus regrets that we are unable to issue a refund.
+</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Change of mind on Donation </h2>
+
+<p>
+Once you have made a donation to sleepbus we cannot give refunds if you change your mind. It is for this reason that we ask that you make your decision to donate to us carefully. sleepbus is under no obligation to give a refund for a donation if an error has been made on your part, but we will endeavour to ensure that any genuine errors (such as the amount donated) are rectified.
+</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Errors by sleepbus</h2>
+
+<p>
+Should an error be made by sleepbus or its financial institution(s), we will make a full refund as soon as possible when notification of the error is received and all costs will be borne by sleepbus.
+</p>
+
+
+<h2 style="text-align: left;padding: 0px;">Contact sleepbus</h2>
+
+<p>
+If you have any queries about the sleepbus Donation Refund Policy, please email <a href="mailto:info@sleepbus.org">info@sleepbus.org</a>
+</p>
+
+
+</div>
+</div>
+
+<div class="fundingtoptext">
+<p><img alt="" src="/application/third_party/ckfinder/userfiles/images/sleepbus%20updated(1).jpg" style="height: 216px; width: 300px;" /></p>
+</div>
+
+
+
+    </div>
+       </div>
+    </div>
+  </div>
+
