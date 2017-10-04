0dc23d0779c8e023fff35139a85fc7e01dac5807
Leon <lionhive@gmail.com>
2017-10-04 23:55:05 +1100

    fix forgot password thanks view

--- src/application/views/thanks/forgot-password-thanks.php
+++ src/application/views/thanks/forgot-password-thanks.php
@@ -1,10 +1,18 @@
-<h1>Forgot Password!</h1>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:135px;">
+  <div class="container">
+    <div class="row">
+			<div class="col-md-2">
+				<img src="/images/simon_thumbs_up.png" style="height:343px;" />
+			</div>
 
-<div class="fundingtoptext">
-<h2>We have sent a reset password link straight to your inbox. Please check your email.</h2>
+			<div class="col-md-8 text-center">
+        <h2>We have sent a reset password link straight to your inbox. Please check your email.</h2>
 
-<p>&nbsp;</p>
+        <p><img alt="" src="/images/userfiles/images/sleepicon.jpg" style="width: 280px; height: 280px;" /></p>
+			</div>
 
-<p><img alt="" src="/images/userfiles/images/sleepicon.jpg" style="width: 280px; height: 280px;" /></p>
+			<div class="col-md-2">
+			</div>
+		</div>
+  </div>
 </div>
-
