8523d2add7c6dcf454d6b92b775b3a2dc83bef2e
Leon <lionhive@gmail.com>
2017-10-05 00:13:47 +1100

    thanks page layout issues #116

--- src/application/views/thanks/newsletter-thanks.php
+++ src/application/views/thanks/newsletter-thanks.php
@@ -1,4 +1,21 @@
-<h1>Thank You</h1>
 
-<h2>You have been successfully subscribed to receive sleepbus emails.</h2>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:135px;">
+  <div class="container">
+    <div class="row">
+			<div class="col-md-2">
+				<img src="/images/simon_thumbs_up.png" style="height:343px;" />
+			</div>
 
+			<div class="col-md-8 text-center">
+        <h1>Thank You</h1>
+
+        <h2>You have been successfully subscribed to receive sleepbus emails.</h2>
+
+        <p><img alt="" src="/images/userfiles/images/sleepicon.jpg" style="width: 280px; height: 280px;" /></p>
+			</div>
+
+			<div class="col-md-2">
+			</div>
+		</div>
+  </div>
+</div>
--- src/application/views/thanks/speaker-request.php
+++ src/application/views/thanks/speaker-request.php
@@ -1,10 +1,20 @@
-<h1>Thanks!</h1>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top:135px;">
+  <div class="container">
+    <div class="row">
+			<div class="col-md-2">
+				<img src="/images/simon_thumbs_up.png" style="height:343px;" />
+			</div>
 
-<div class="fundingtoptext">
-<h2>Thank you for connecting with sleepbus. We will be in touch with you shortly.</h2>
+			<div class="col-md-8 text-center">
+        <h1>Thanks!</h1>
 
-<p>&nbsp;</p>
+        <h2>Thank you for connecting with sleepbus. We will be in touch with you shortly.</h2>
 
-<p><img alt="" src="/images/userfiles/images/sleepicon.jpg" style="width: 280px; height: 280px;" /></p>
+        <p><img alt="" src="/images/userfiles/images/sleepicon.jpg" style="width: 280px; height: 280px;" /></p>
+			</div>
+
+			<div class="col-md-2">
+			</div>
+		</div>
+  </div>
 </div>
-
